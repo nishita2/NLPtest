b926a8e8c4af71cb41a4573fc39e679af9cda21f
nishita2 <nishimura-tatsurou@ed.tmu.ac.jp>
2014-10-27 22:16:43 +0900

    test1027

--- set_2.py
+++
@@ -1,6 +0,0 @@
-#coding: UTF-8
-
-file = open('sample.txt')
-txt = file.read()
-a = txt.split("\t")
-print ' '.join(a)
--- set_7.py
+++
@@ -1,9 +0,0 @@
-#coding: UTF-8
-
-file = open('sample.txt','r')
-txt = file.readlines()
-first = []
-for row in txt:
-    a = row.split('\t')
-    first.append(a[0])
-print len(set(first))
